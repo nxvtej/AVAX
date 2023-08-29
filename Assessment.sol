947bc8d9d42b98acbc0cf3d61e8a4332eec00a90
Navdeep <108007267+nxvtej@users.noreply.github.com>
2023-08-29 15:33:55 +0530

    Update Assessment.sol

--- Assessment.sol
+++ Assessment.sol
@@ -1,20 +1,31 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
 
-contract AvengersDonation {
-    mapping(address => uint256) public balances;
+//import "hardhat/console.sol";
 
-    function deposit() public payable {
-        balances[msg.sender] += msg.value;
+contract Assessment {
+    address payable public owner;
+    uint256 public balance;
+
+    event Deposit(uint256 amount);
+    event Donate(uint256 amount,address payable reciever);
+
+    constructor(uint initBalance) payable {
+        owner = payable(msg.sender);
+        balance = initBalance;
     }
 
-    function withdraw(uint256 _amount) public {
-        require(balances[msg.sender] >= _amount, "Insufficient balance");
-        balances[msg.sender] -= _amount;
-        payable(msg.sender).transfer(_amount);
+    function getBalance() public view returns(uint256){
+        return balance;
     }
 
-    function getBalance() public view returns (uint256) {
-        return balances[msg.sender];
+    function deposit(uint256 _amount) public payable {
+        balance += _amount;
+        emit Deposit(_amount);
+    }
+
+     function donate(uint256 donateAmount , address payable reciever) public payable{    
+        balance = balance - donateAmount;
+        emit Donate(donateAmount , reciever );
     }
 }
